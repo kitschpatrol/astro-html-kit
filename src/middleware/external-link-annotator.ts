41d933e9d83fd8d90154b6c9fd77f0e6229e7013
Eric Mika <eric@ericmika.com>
2026-04-15 20:09:53 -0400

    Narrow external link detection.

--- src/middleware/external-link-annotator.ts
+++ src/middleware/external-link-annotator.ts
@@ -3,11 +3,12 @@ import { defineDomMiddleware } from './dom-middleware'
 export const externalLinkAnnotator = defineDomMiddleware((context, document) => {
 	const localHostname = 'localhost'
 	const { hostname: ourHostname } = context.site ?? { hostname: '' }
-	// Not on hero pages
-	for (const element of document.querySelectorAll<HTMLAnchorElement>('a')) {
+	// TODO Not on hero pages for starlight?
+	for (const element of document.querySelectorAll<HTMLAnchorElement>(
+		'a[href]:not([href^="#"]):not([href^="/"])',
+	)) {
 		try {
 			const { hostname } = new URL(element.href)
-			console.log(hostname)
 			if (hostname !== ourHostname && hostname !== localHostname && hostname !== '') {
 				element.dataset.externalLink = ''
 				element.setAttribute('rel', 'noopener noreferrer')
