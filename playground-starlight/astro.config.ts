571ca3598f6412fae5ea0dc5142ecb2b235c49c7
Eric Mika <eric@ericmika.com>
2026-06-11 00:41:01 -0400

    Require Astro 6.4. Update dependencies.

--- playground-starlight/astro.config.ts
+++ playground-starlight/astro.config.ts
@@ -9,20 +9,6 @@ export default defineConfig({
 					items: [{ label: 'Middleware', slug: 'middleware' }],
 					label: 'Tests',
 				},
-				{
-					items: [
-						{ label: 'Picture', slug: 'components/picture' },
-						{ label: 'Video', slug: 'components/video' },
-						{ label: 'Audio', slug: 'components/audio' },
-						{ label: 'Caption', slug: 'components/caption' },
-						{ label: 'Zoom', slug: 'components/zoom' },
-					],
-					label: 'Components',
-				},
-				{
-					items: [{ label: 'Syntax & Directives', slug: 'mdx/syntax' }],
-					label: 'MDX',
-				},
 			],
 			title: 'astro-html-kit',
 		}),
